c51633b6a5ebd85c58b90bdb2201618efe9eb101
muhsinwazza <muhsinwazza@gmail.com>
2017-07-17 15:37:23 +0530

    Localised ml-In
    For Price

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-
-
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace MvcAppFirst.Models
 {
     public class Movies
     {
-       
+            
+            
             public int ID { get; set; }
+            [Required]
             public string Title { get; set; }
+            [DataType(DataType.DateTime),Required]
             public DateTime ReleaseDate { get; set; }
+            [Required]
             public string Genre { get; set; }
+            [Required,Range(1,100),DataType(DataType.Currency)]
             public decimal Price { get; set; }
-       
+            [Required]
+            public string Rating { get; set; }
         
 
     }
